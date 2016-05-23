c15749db71a5c27e6adfb6ba1c767f8ff7917f8f
xiayuanchuan <772321344@qq.com>
2016-05-23 17:48:56 +0800

    数据库配置    默认头像

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,17 +7,20 @@ use Think\Controller;
 class IndexController extends FrontBaseController {
 
     public function index() {
-        $userModel = M("pintuan_order");
+        $userModel = M("member");
         $users=S("users");
         if(!$users){
             $users=$userModel->cache("users",60)->select();
         }
-//        dump($users);
+        
+        
+        
+        dump($users);
 //        
 //        $image = new \Think\Image();
 //        $image->open('./qiguai.jpg');
 //        $image->thumb(200, 200,\Think\Image::IMAGE_THUMB_CENTER)->save('./ceshi.jpg');
-//        $this->display();
+        $this->display();
     }
 
 }
